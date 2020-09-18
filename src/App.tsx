c61813ab99481425c364f06f55b551fafb80f26b
chacha <867571123@qq.com>
2020-09-18 15:18:36 +0800

    feat: init a react threejs project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FunctionComponent, useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { ControlsProvider, Controls, useControl } from 'react-three-gui';
+
 import './App.css';
 
-function App() {
+const Mesh: FunctionComponent<any> = (props) => {
+  const mesh = useRef<any>();
+  const rotationX = useControl('Rotation X', { type: 'number' });
+  const rotationY = useControl('Rotation Y', { type: 'number' });
+
+  useFrame(() => {
+    // mesh.current.rotation.x = mesh.current.rotation.y += 0.02;
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      receiveShadow
+      scale={[1.5, 1.5, 1.5]}
+      rotation-x={rotationX}
+      rotation-y={rotationY}
+      position={[0, 0, 0]}
+    >
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+};
+
+function App(props: any) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ControlsProvider>
+        <Canvas style={{ height: '100vh' }} colorManagement>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Mesh />
+        </Canvas>
+        <Controls />
+      </ControlsProvider>
     </div>
   );
 }
