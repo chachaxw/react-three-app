296bd03282fb5e2ad34895774172256bce58115c
chacha <867571123@qq.com>
2020-09-20 16:24:12 +0800

    feat: add orbitControls JSX declare

---
+++ src/typings/types.d.ts
@@ -0,0 +1,9 @@
+import { ReactThreeFiber } from 'react-three-fiber';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      orbitControls: ReactThreeFiber.Object3DNode<OrbitControls, typeof OrbitControls>;
+    }
+  }
+}
