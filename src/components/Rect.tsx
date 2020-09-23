289596412a77a5ff22b207085b9cee4db9dd5669
chacha <867571123@qq.com>
2020-09-23 22:17:59 +0800

    feat: update Rect component

--- src/components/Rect.tsx
+++ src/components/Rect.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, MutableRefObject, useMemo, useRef } from 'react';
+import React, { FunctionComponent } from 'react';
 import { useLoader } from 'react-three-fiber';
 import { DoubleSide, Texture, TextureLoader } from 'three';
 
@@ -8,13 +8,13 @@ interface Props {
 }
 
 const Rect: FunctionComponent<Props> = (props) => {
-  const { url, positionZ } = props;
+  const { url } = props;
   const texture = useLoader<Texture>(TextureLoader, url);
 
   return (
     <mesh castShadow receiveShadow rotation={[0, 0, 0]} position={[0, 0, 0]}>
       <planeGeometry attach="geometry" args={[1, 1]} />
-      <meshStandardMaterial attach="material" map={texture} side={DoubleSide} opacity={0.8} />
+      <meshStandardMaterial attach="material" map={texture} side={DoubleSide} opacity={0.5} />
     </mesh>
   );
 };
